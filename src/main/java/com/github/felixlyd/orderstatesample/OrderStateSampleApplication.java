9b3c503e77188fb45b56b6883486119685a3f903
YaodongLiu <ttttttliu@qq.com>
2023-04-25 16:30:15 +0800

    Initial commit

---
+++ src/main/java/com/github/felixlyd/orderstatesample/OrderStateSampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.felixlyd.orderstatesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderStateSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderStateSampleApplication.class, args);
+    }
+
+}
